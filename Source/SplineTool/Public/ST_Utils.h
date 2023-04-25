9217960439090020e2a95d622ce9ff947b0c6511
LeShrexpert <61854276+LeReel@users.noreply.github.com>
2023-04-25 16:34:36 +0200

    Updated DEBUG macros

--- Source/SplineTool/Public/ST_Utils.h
+++ Source/SplineTool/Public/ST_Utils.h
@@ -2,7 +2,7 @@
 
 DECLARE_LOG_CATEGORY_EXTERN(LogCustom, Log, All);
 
-#define DEBUG_D(owner, text) UE_LOG(LogCustom, Display, TEXT(text))
-#define DEBUG_W(owner, text) UE_LOG(LogCustom, Warning, TEXT(text))
-#define DEBUG_E(owner, text) UE_LOG(LogCustom, Error, TEXT(text))
-#define DEBUG_F(owner, text) UE_LOG(LogCustom, Fatal, TEXT(text))
+#define DEBUG_D(text) UE_LOG(LogCustom, Display, TEXT(text))
+#define DEBUG_W(text) UE_LOG(LogCustom, Warning, TEXT(text))
+#define DEBUG_E(text) UE_LOG(LogCustom, Error, TEXT(text))
+#define DEBUG_F(text) UE_LOG(LogCustom, Fatal, TEXT(text))
