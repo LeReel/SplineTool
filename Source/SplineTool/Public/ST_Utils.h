5825df397ede256cb6e12592f3cf773b49efadbf
LeShrexpert <61854276+LeReel@users.noreply.github.com>
2023-04-27 16:09:07 +0200

    Added params to Debugs macros

--- Source/SplineTool/Public/ST_Utils.h
+++ Source/SplineTool/Public/ST_Utils.h
@@ -2,7 +2,7 @@
 
 DECLARE_LOG_CATEGORY_EXTERN(LogCustom, Log, All);
 
-#define DEBUG_D(text) UE_LOG(LogCustom, Display, TEXT(text))
-#define DEBUG_W(text) UE_LOG(LogCustom, Warning, TEXT(text))
-#define DEBUG_E(text) UE_LOG(LogCustom, Error, TEXT(text))
-#define DEBUG_F(text) UE_LOG(LogCustom, Fatal, TEXT(text))
+#define DEBUG_D(text, ...) UE_LOG(LogTemp, Display, TEXT(text), __VA_ARGS__);
+#define DEBUG_W(text, ...) UE_LOG(LogTemp, Warning, TEXT(text), __VA_ARGS__);
+#define DEBUG_E(text, ...) UE_LOG(LogTemp, Error, TEXT(text), __VA_ARGS__);
+#define DEBUG_F(text, ...) UE_LOG(LogTemp, Fatal, TEXT(text), __VA_ARGS__);
